eaf659369645fe1dee8b3bf6886067b4bb9d579c
Nahida Akther <ummenahida20@gmail.com>
2026-02-14 17:40:11 +0600

    fix: get product items

---
+++ product.js
@@ -0,0 +1,21 @@
+
+
+const cart = [
+  { id: 1, name: "Laptop", price: 50000, quantity: 1 },
+  { id: 2, name: "Mouse", price: 500, quantity: 2 },
+  { id: 3, name: "Keyboard", price: 1500, quantity: 1 },
+  { id: 2, name: "Mouse", price: 500, quantity: 1 }
+];
+
+
+function getTotalProduct(){
+  let sum = 0;
+  console.log("helo")
+  for(let i=0; i <= cart.length; i++){
+       console.log("hello ami sunte pacci tmk")
+  }
+}
+
+
+const result = getTotalProduct()
+console.log(cart.length)
