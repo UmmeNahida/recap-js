e86652f859c9f37394d8fbb2400ef5af410ffe5b
Nahida Akther <ummenahida20@gmail.com>
2026-02-14 18:34:57 +0600

    get unique item

--- product.js
+++ product.js
@@ -7,15 +7,28 @@ const cart = [
   { id: 2, name: "Mouse", price: 500, quantity: 1 }
 ];
 
+const cart2 = { id: 1, name: "Laptop", price: 50000, quantity: 1 }
+const friends = ["ayesh", "nahida", "mahir"]
+
+console.log(friends[1])
+
+
+let uniqueProduct = []
 
 function getTotalProduct(){
+  const result = cart.map((product)=> {
+    if(!uniqueProduct.includes(product.id)){
+        uniqueProduct.push(product)
+    }
+
+    
+  })
+
+  return uniqueProduct
   
-  const result = cart.map((product)=> product.price)
-  return result
 }
 
 
 const result = getTotalProduct()
-console.log(result)
-console.log(cart.length)
+// console.log("result",result)
 
