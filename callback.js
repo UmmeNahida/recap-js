194f69cb0ef0e008234a9e2f1f188295f8e9733e
Nahida Akther <ummenahida20@gmail.com>
2026-02-22 23:40:43 +0600

    chore: practice callback funtion and how does it work

---
+++ callback.js
@@ -0,0 +1,30 @@
+const API = "https://api.codeababil.com/secret-key";
+ 
+
+const handleFetch = async (callback) => {
+  try {
+    const response = await fetch(API, {
+      method:"GET", 
+      headers:{
+      'Content-Type':"application/json"
+      }
+    });
+
+    const data = await response.json(); // Task
+      
+    if(typeof callback !== 'function') return;
+
+    const result =  callback(data);
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+handleFetch((data) =>{
+  // console.log("I love Programming!")
+  // console.log(data);
+
+  return {...data, message: "I love programming!"};
+})
