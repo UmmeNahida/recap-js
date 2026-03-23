a017d0cfe720bec67418c05b13524ce015155677
Nahida Akther <ummenahida20@gmail.com>
2026-03-23 23:58:54 +0600

    feat: add cart array and totalPrice function skeleton

---
+++ problem-solving.js
@@ -0,0 +1,9 @@
+const cart = [
+  { name: "Pen", price: 10, quantity: 2 },
+  { name: "Book", price: 50, quantity: 1 },
+  { name: "Bag", price: 100, quantity: 3 }
+];
+
+const totalPrice =()=>{
+  
+}
