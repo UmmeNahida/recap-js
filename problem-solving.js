7c6bff2bccd41ccfff4c548361dd288127d9fb45
Nahida Akther <ummenahida20@gmail.com>
2026-03-25 23:58:28 +0600

    fix: clean up commented code and ensure proper formatting

--- problem-solving.js
+++ problem-solving.js
@@ -1,12 +1,18 @@
+
+
 const cart = [
   { name: "Pen", price: 10, quantity: 2 },
   { name: "Book", price: 50, quantity: 1 },
   { name: "Bag", price: 100, quantity: 3 }
 ];
 
-const totalPrice =()=>{
 
-}
+// const getTotalPrice = ()=>{
+//   const result = cart.
+// }
 
-// problem 2
-const nums = [1,2,3,2,4,5,1,6];
+// // problem 2
+// const nums = [1,2,3,2,4,5,1,6];
+
+// // ✅ Task 3: Capitalize first letter
+// const str = "hello world";
